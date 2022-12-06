469e08f7035d64a5fdba010aa8ef7d17721e52d4
Francisco Silva <fmsilva1996@gmail.com>
2022-12-06 23:12:17 +0000

    feat: completed challenge 1 (part 2)

--- src/01.ts
+++ src/01.ts
@@ -6,7 +6,8 @@ const formattedElfCaloryList = elfCaloryList
   .split(/\n\n/)
   .map((elf) => elf.split(/\n/));
 
-const addUpCalories = (elfCalories: string[]) =>
+// * PART 1 - Elf with Most Calories
+const addUpCalories = (elfCalories: (string | number)[]) =>
   elfCalories.reduce<number>((acc, cur) => (acc += Number(cur)), 0);
 
 const elfTotalCalories = formattedElfCaloryList.map(addUpCalories);
@@ -14,3 +15,12 @@ const elfTotalCalories = formattedElfCaloryList.map(addUpCalories);
 const maxElfCalories = Math.max(...elfTotalCalories);
 
 console.log("🚀 ~ maxElfCalories:", maxElfCalories);
+
+// * PART 2 - Top 3 Elves
+const elvesSortedByTotalCalories = elfTotalCalories.sort((a, b) => b - a);
+
+const topThreeElves = elvesSortedByTotalCalories.slice(0, 3);
+
+const topThreeElvesCalorySum = addUpCalories(topThreeElves);
+
+console.log("🚀 ~ topThreeElvesCalorySum", topThreeElvesCalorySum);
